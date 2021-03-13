d0aa239d0e804f777b587d7d4c021751a9257ced
Vlatko <vladimir.icovski@gmail.com>
2021-03-13 10:22:05 -0500

    Click to open dropdown added, visual needs fixed

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let svg= document.querySelector("#Capa_1")
 svg.addEventListener("click", e => {console.log(e);})
+
+var menu_button = document.querySelector("#menu-button");
+var menu = document.querySelector("#menu");
+menu_button.addEventListener("click", e => {
+  console.log(e);
+  menu.classList.toggle("menu-open");
+});
