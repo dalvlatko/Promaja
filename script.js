df250e93ec739611a3fb70022380978928aeab28
Replit user <replituser@example.com>
2021-03-15 16:19:02 +0000

    Cleaned up code for dropdown

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let svg= document.querySelector("#Capa_1")
-svg.addEventListener("click", e => {console.log(e);})
-
 var menu_button = document.querySelector("#menu-button");
 var menu = document.querySelector("#menu");
 menu_button.addEventListener("click", e => {
   console.log(e);
   menu.classList.toggle("menu-open");
 });
-menu.addEventListener("click", e => {
-  console.log(e);
-  menu.classList.toggle("menu-open");
-});
+
